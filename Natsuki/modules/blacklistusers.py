9e216de3bcecd2ed94aa4e89ecfaa291e3490785
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 22:58:59 -0700

    Updated

--- Natsuki/modules/blacklistusers.py
+++ Natsuki/modules/blacklistusers.py
@@ -6,14 +6,14 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
-import EzilaXBotV.modules.sql.blacklistusers_sql as sql
-from EzilaXBotV import DEMONS, DEV_USERS, DRAGONS, OWNER_ID, TIGERS, WOLVES, dispatcher
-from EzilaXBotV.modules.helper_funcs.chat_status import dev_plus
-from EzilaXBotV.modules.helper_funcs.extraction import (
+import Natsuki.modules.sql.blacklistusers_sql as sql
+from Natsuki import DEMONS, DEV_USERS, DRAGONS, OWNER_ID, TIGERS, WOLVES, dispatcher
+from Natsuki.modules.helper_funcs.chat_status import dev_plus
+from Natsuki.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from EzilaXBotV.modules.log_channel import gloggable
+from Natsuki.modules.log_channel import gloggable
 
 BLACKLISTWHITELIST = [OWNER_ID] + DEV_USERS + DRAGONS + WOLVES + DEMONS
 BLABLEUSERS = [OWNER_ID] + DEV_USERS
