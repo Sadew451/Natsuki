9b44788127161a9b8d32771914ba1a083579ac49
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 19:43:03 -0700

    Updated

--- Natsuki/modules/sql/__init__.py
+++ Natsuki/modules/sql/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from EzilaXBotV import DB_URI
+from Natsuki import DB_URI
 
 
 def start() -> scoped_session:
