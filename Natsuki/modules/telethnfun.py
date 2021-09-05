2e723ec2712dce2b701bb8bea9f8c337463a8b6f
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 12:56:41 +0530

    Natsuki_Updated

---
+++ Natsuki/modules/telethnfun.py
@@ -0,0 +1,175 @@
+import glob
+import os
+from asyncio import sleep
+from datetime import datetime
+
+import html2text
+import requests
+from bing_image_downloader import downloader
+from requests import get, post
+from telethon.tl import functions, types
+
+from EzilaXBotV import telethn as client
+from EzilaXBotV.events import register
+
+
+def progress(current, total):
+    """Calculate and return the download progress with given arguments."""
+    print(
+        "Downloaded {} of {}\nCompleted {}".format(
+            current, total, (current / total) * 100
+        )
+    )
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+
+        return isinstance(
+            (
+                await client(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    elif isinstance(chat, types.InputPeerChat):
+
+        ui = await client.get_peer_id(user)
+        ps = (
+            await client(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    else:
+        return None
+
+
+@register(pattern="^/gs (.*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.is_group:
+        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+            await event.reply(
+                " Hai.. You are not admin..  You can't use this command.. But you can use in my pm"
+            )
+            return
+    # SHOW_DESCRIPTION = False
+    input_str = event.pattern_match.group(
+        1
+    )  # + " -inurl:(htm|html|php|pls|txt) intitle:index.of \"last modified\" (mkv|mp4|avi|epub|pdf|mp3)"
+    input_url = "https://bots.shrimadhavuk.me/search/?q={}".format(input_str)
+    headers = {"USER-AGENT": "UniBorg"}
+    response = requests.get(input_url, headers=headers).json()
+    output_str = " "
+    for result in response["results"]:
+        text = result.get("title")
+        url = result.get("url")
+        description = result.get("description")
+        last = html2text.html2text(description)
+        output_str += "[{}]({})\n{}\n".format(text, url, last)
+    await event.reply(
+        "{}".format(output_str), link_preview=False, parse_mode="Markdown"
+    )
+
+
+@register(pattern="^/img (.*)")
+async def img_sampler(event):
+    if event.fwd_from:
+        return
+    if event.is_group:
+        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+            await event.reply(".. You are not admin.. use in bot  pm")
+            return
+    query = event.pattern_match.group(1)
+    jit = f'"{query}"'
+    downloader.download(
+        jit,
+        limit=5,
+        output_dir="store",
+        adult_filter_off=False,
+        force_replace=False,
+        timeout=60,
+    )
+    os.chdir(f'./store/"{query}"')
+    types = ("*.png", "*.jpeg", "*.jpg")  # the tuple of file types
+    files_grabbed = []
+    for files in types:
+        files_grabbed.extend(glob.glob(files))
+    await event.client.send_file(event.chat_id, files_grabbed, reply_to=event.id)
+    os.remove(files_grabbed)
+    os.chdir("./")
+
+
+@register(pattern=r"^/getqr$")
+async def parseqr(qr_e):
+    """For .getqr command, get QR Code content from the replied photo."""
+    if qr_e.fwd_from:
+        return
+    start = datetime.now()
+    downloaded_file_name = await qr_e.client.download_media(
+        await qr_e.get_reply_message(), progress_callback=progress
+    )
+    url = "https://api.qrserver.com/v1/read-qr-code/?outputformat=json"
+    file = open(downloaded_file_name, "rb")
+    files = {"file": file}
+    resp = post(url, files=files).json()
+    qr_contents = resp[0]["symbol"][0]["data"]
+    file.close()
+    os.remove(downloaded_file_name)
+    end = datetime.now()
+    duration = (end - start).seconds
+    await qr_e.reply(
+        "Obtained QRCode contents in {} seconds.\n{}".format(duration, qr_contents)
+    )
+
+
+@register(pattern=r"^/makeqr(?: |$)([\s\S]*)")
+async def make_qr(qrcode):
+    """For .makeqr command, make a QR Code containing the given content."""
+    if qrcode.fwd_from:
+        return
+    start = datetime.now()
+    input_str = qrcode.pattern_match.group(1)
+    message = "SYNTAX: `.makeqr <long text to include>`"
+    reply_msg_id = None
+    if input_str:
+        message = input_str
+    elif qrcode.reply_to_msg_id:
+        previous_message = await qrcode.get_reply_message()
+        reply_msg_id = previous_message.id
+        if previous_message.media:
+            downloaded_file_name = await qrcode.client.download_media(
+                previous_message, progress_callback=progress
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as file:
+                m_list = file.readlines()
+            message = ""
+            for media in m_list:
+                message += media.decode("UTF-8") + "\r\n"
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+
+    url = "https://api.qrserver.com/v1/create-qr-code/?data={}&\
+size=200x200&charset-source=UTF-8&charset-target=UTF-8\
+&ecc=L&color=0-0-0&bgcolor=255-255-255\
+&margin=1&qzone=0&format=jpg"
+
+    resp = get(url.format(message), stream=True)
+    required_file_name = "temp_qr.webp"
+    with open(required_file_name, "w+b") as file:
+        for chunk in resp.iter_content(chunk_size=128):
+            file.write(chunk)
+    await qrcode.client.send_file(
+        qrcode.chat_id,
+        required_file_name,
+        reply_to=reply_msg_id,
+        progress_callback=progress,
+    )
+    os.remove(required_file_name)
+    duration = (datetime.now() - start).seconds
+    await qrcode.reply("Created QRCode in {} seconds".format(duration))
+    await sleep(5)
---
+++ Natsuki/modules/tts.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from gtts import gTTS
+from telegram import ChatAction, Update
+from telegram.ext import CallbackContext, run_async
+
+from EzilaXBotV import dispatcher
+from EzilaXBotV.modules.disable import DisableAbleCommandHandler
+
+
+@run_async
+def tts(update: Update, context: CallbackContext):
+    args = context.args
+    current_time = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M:%S")
+    datetime.now().strftime("%d%m%y-%H%M%S%f")
+    reply = " ".join(args)
+    update.message.chat.send_action(ChatAction.RECORD_AUDIO)
+    lang = "ml"
+    tts = gTTS(reply, lang)
+    tts.save("k.mp3")
+    with open("k.mp3", "rb") as f:
+        linelist = list(f)
+        linecount = len(linelist)
+    if linecount == 1:
+        update.message.chat.send_action(ChatAction.RECORD_AUDIO)
+        lang = "en"
+        tts = gTTS(reply, lang)
+        tts.save("k.mp3")
+    with open("k.mp3", "rb") as speech:
+        update.message.reply_voice(speech, quote=False)
+
+
+TTS_HANDLER = DisableAbleCommandHandler("tts", tts, pass_args=True)
+dispatcher.add_handler(TTS_HANDLER)
+
+__command_list__ = ["tts"]
+__handlers__ = [TTS_HANDLER]
