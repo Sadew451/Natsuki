4d263501ccb17e37312b65121ebb1f8e1af8ec0c
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 19:49:11 -0700

    Update global_bans_sql.py

--- Natsuki/modules/sql/global_bans_sql.py
+++ Natsuki/modules/sql/global_bans_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText
 
-from EzilaXBotV.modules.sql import BASE, SESSION
+from Natsuki.modules.sql import BASE, SESSION
 
 
 class GloballyBannedUsers(BASE):
