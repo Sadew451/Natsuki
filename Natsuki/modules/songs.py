3f252d655778b570af0888bdf6cfb3c11390ffac
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 12:27:14 +0530

    Natsuki_Updated

---
+++ Natsuki/modules/songs.py
@@ -0,0 +1,86 @@
+import os
+
+import aiohttp
+from pyrogram import filters
+from pytube import YouTube
+from youtubesearchpython import VideosSearch
+
+from EzilaXBotV import LOGGER, pbot
+from EzilaXBotV.utils.uut import get_arg
+
+
+def yt_search(song):
+    videosSearch = VideosSearch(song, limit=1)
+    result = videosSearch.result()
+    if not result:
+        return False
+    else:
+        video_id = result["result"][0]["id"]
+        url = f"https://youtu.be/{video_id}"
+        return url
+
+
+class AioHttp:
+    @staticmethod
+    async def get_json(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.json()
+
+    @staticmethod
+    async def get_text(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.text()
+
+    @staticmethod
+    async def get_raw(link):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(link) as resp:
+                return await resp.read()
+
+
+@pbot.on_message(filters.command("song"))
+async def song(client, message):
+    message.chat.id
+    user_id = message.from_user["id"]
+    args = get_arg(message) + " " + "song"
+    if args.startswith(" "):
+        await message.reply("Enter a song name. Check /help")
+        return ""
+    status = await message.reply("Processing...")
+    video_link = yt_search(args)
+    if not video_link:
+        await status.edit("Song not found.")
+        return ""
+    yt = YouTube(video_link)
+    audio = yt.streams.filter(only_audio=True).first()
+    try:
+        download = audio.download(filename=f"{str(user_id)}")
+    except Exception as ex:
+        await status.edit("Failed to download song")
+        LOGGER.error(ex)
+        return ""
+    os.rename(download, f"{str(user_id)}.mp3")
+    await pbot.send_chat_action(message.chat.id, "upload_audio")
+    await pbot.send_audio(
+        chat_id=message.chat.id,
+        audio=f"{str(user_id)}.mp3",
+        duration=int(yt.length),
+        title=str(yt.title),
+        performer=str(yt.author),
+        reply_to_message_id=message.message_id,
+    )
+    await status.delete()
+    os.remove(f"{str(user_id)}.mp3")
+
+
+__help__ = """
+ *You can either enter just the song name or both the artist and song
+  name. *
+  /song <songname artist(optional)>*:* uploads the song in it's best quality available
+  /video <songname artist(optional)>*:* uploads the video song in it's best quality available
+  /lyrics <song>*:* returns the lyrics of that song.
+"""
+
+__mod_name__ = "Music"
---
+++ Natsuki/modules/stt.py
@@ -0,0 +1,98 @@
+import os
+from datetime import datetime
+
+import requests
+from telethon import *
+from telethon.tl import functions, types
+from telethon.tl.types import *
+
+from EzilaXBotV import *
+from EzilaXBotV import telethn
+from EzilaXBotV.events import register
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+        return isinstance(
+            (
+                await telethn(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerChat):
+        ui = await telethn.get_peer_id(user)
+        ps = (
+            await telethn(functions.messages.GetFullChatRequest(chat.chat_id))
+        ).full_chat.participants.participants
+        return isinstance(
+            next((p for p in ps if p.user_id == ui), None),
+            (types.ChatParticipantAdmin, types.ChatParticipantCreator),
+        )
+    return False
+
+
+@register(pattern="^/stt$")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.is_group:
+        if not (await is_register_admin(event.input_chat, event.message.sender_id)):
+            await event.reply(
+                " Hi.. You are not admin.. You can't use this command.. But you can use in my pm"
+            )
+            return
+
+    start = datetime.now()
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        required_file_name = await event.client.download_media(
+            previous_message, TEMP_DOWNLOAD_DIRECTORY
+        )
+        if IBM_WATSON_CRED_URL is None or IBM_WATSON_CRED_PASSWORD is None:
+            await event.reply(
+                "You need to set the required ENV variables for this module. \nModule stopping"
+            )
+        else:
+            await event.reply("Starting analysis")
+            headers = {
+                "Content-Type": previous_message.media.document.mime_type,
+            }
+            data = open(required_file_name, "rb").read()
+            response = requests.post(
+                IBM_WATSON_CRED_URL + "/v1/recognize",
+                headers=headers,
+                data=data,
+                auth=("apikey", IBM_WATSON_CRED_PASSWORD),
+            )
+            r = response.json()
+            if "results" in r:
+                # process the json to appropriate string format
+                results = r["results"]
+                transcript_response = ""
+                transcript_confidence = ""
+                for alternative in results:
+                    alternatives = alternative["alternatives"][0]
+                    transcript_response += " " + str(alternatives["transcript"])
+                    transcript_confidence += (
+                        " " + str(alternatives["confidence"]) + " + "
+                    )
+                end = datetime.now()
+                ms = (end - start).seconds
+                if transcript_response != "":
+                    string_to_show = "Language: `English`\nTRANSCRIPT: `{}`\nTime Taken: {} seconds\nConfidence: `{}`".format(
+                        transcript_response, ms, transcript_confidence
+                    )
+                else:
+                    string_to_show = "Language: `English`\nTime Taken: {} seconds\n**No Results Found**".format(
+                        ms
+                    )
+                await event.reply(string_to_show)
+            else:
+                await event.reply(r["error"])
+            # now, remove the temporary file
+            os.remove(required_file_name)
+    else:
+        await event.reply("Reply to a voice message, to get the text out of it.")
