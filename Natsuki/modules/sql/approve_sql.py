441d3f508d69f54bfbf0c59997e397f41d08a2b0
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 19:44:35 -0700

    Updated

--- Natsuki/modules/sql/approve_sql.py
+++ Natsuki/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, String
 
-from EzilaXBotV.modules.sql import BASE, SESSION
+from Natsuki.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
