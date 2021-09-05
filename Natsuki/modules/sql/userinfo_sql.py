af39c8a36572d3e6fbf4b59a473812c536b27325
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 19:51:55 -0700

    Update userinfo_sql.py

--- Natsuki/modules/sql/userinfo_sql.py
+++ Natsuki/modules/sql/userinfo_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, UnicodeText
 
-from EzilaXBotV.modules.sql import BASE, SESSION
+from Natsuki.modules.sql import BASE, SESSION
 
 
 class UserInfo(BASE):
