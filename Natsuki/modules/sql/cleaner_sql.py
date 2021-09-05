978e12aa3688665f4beb7658727d5e21debb582a
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 19:46:20 -0700

    Updated

--- Natsuki/modules/sql/cleaner_sql.py
+++ Natsuki/modules/sql/cleaner_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Boolean, Column, UnicodeText
 
-from EzilaXBotV.modules.sql import BASE, SESSION
+from Natsuki.modules.sql import BASE, SESSION
 
 
 class CleanerBlueTextChatSettings(BASE):
