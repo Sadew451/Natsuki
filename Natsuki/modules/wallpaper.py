acc7d178a5eb22f94e3544f583aca2a543456387
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 00:40:01 -0700

    Updated

--- Natsuki/modules/wallpaper.py
+++ Natsuki/modules/wallpaper.py
@@ -4,8 +4,8 @@ import requests as r
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
-from EzilaXBotV import SUPPORT_CHAT, WALL_API, dispatcher
-from EzilaXBotV.modules.disable import DisableAbleCommandHandler
+from Natsuki import SUPPORT_CHAT, WALL_API, dispatcher
+from Natsuki.modules.disable import DisableAbleCommandHandler
 
 # Wallpapers module by @TheRealPhoenix using wall.alphacoders.com
 
