25fbea7e54f27225e8b738f09933a1d4e1d008d8
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 23:31:46 -0700

    Updated

--- Natsuki/modules/log_channel.py
+++ Natsuki/modules/log_channel.py
@@ -3,7 +3,7 @@ from functools import wraps
 
 from telegram.ext import CallbackContext
 
-from EzilaXBotV.modules.helper_funcs.misc import is_module_loaded
+from Natsuki.modules.helper_funcs.misc import is_module_loaded
 
 FILENAME = __name__.rsplit(".", 1)[-1]
 
@@ -13,9 +13,9 @@ if is_module_loaded(FILENAME):
     from telegram.ext import CommandHandler, JobQueue, run_async
     from telegram.utils.helpers import escape_markdown
 
-    from EzilaXBotV import EVENT_LOGS, LOGGER, dispatcher
-    from EzilaXBotV.modules.helper_funcs.chat_status import user_admin
-    from EzilaXBotV.modules.sql import log_channel_sql as sql
+    from Natsuki import EVENT_LOGS, LOGGER, dispatcher
+    from Natsuki.modules.helper_funcs.chat_status import user_admin
+    from Natsuki.modules.sql import log_channel_sql as sql
 
     def loggable(func):
         @wraps(func)
