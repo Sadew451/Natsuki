f46d7a32e8090b589d10624e0d4594ecf783d5a6
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 11:12:29 +0530

    Natsuki_Updated

---
+++ Natsuki/modules/about.py
@@ -0,0 +1,14 @@
+__help__ = """
+** EzilaXBotV - A Powerful Telegram Group Manager 🎶 **
+
+Powerful Abilities
+
+• Group Voice Chat Music Play ❤️
+
+• File To Link and URL Upload 📂
+
+• Youtube Downloader 🎵
+
+Developer [Sadew🇱🇰](https://t.me/Darkridersslk)
+"""
+__mod_name__ = "About"
---
+++ Natsuki/modules/animequotes.py
@@ -0,0 +1,19 @@
+# Made By @Madepranav On Telegram & Github Id Superboyfan
+import random
+
+from telegram import Update
+from telegram.ext import CallbackContext, run_async
+
+import EzilaXBotV.modules.animequotesstring as animequotesstring
+from EzilaXBotV import dispatcher
+from EzilaXBotV.modules.disable import DisableAbleCommandHandler
+
+
+@run_async
+def animequotes(update: Update, context: CallbackContext):
+    update.effective_message.reply_text(random.choice(animequotesstring.ANIMEQUOTES))
+
+
+ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("animequotes", animequotes)
+
+dispatcher.add_handler(ANIMEQUOTES_HANDLER)
