9c4f2358ebf8c16f845e22701720f4f36313338d
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 11:34:04 +0530

    Natsuki_Updated

---
+++ Natsuki/modules/covid.py
@@ -0,0 +1,24 @@
+import requests
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
+
+from EzilaXBotV import dispatcher
+from EzilaXBotV.modules.disable import DisableAbleCommandHandler
+
+
+@run_async
+def covid(update: Update, context: CallbackContext):
+    message = update.effective_message
+    text = message.text.split(" ", 1)
+    if len(text) == 1:
+        r = requests.get("https://corona.lmao.ninja/v2/all").json()
+        reply_text = f"**Global Totals** 🦠\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+    else:
+        variabla = text[1]
+        r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
+        reply_text = f"**Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
+
+
+COVID_HANDLER = DisableAbleCommandHandler(["covid", "corona"], covid)
+dispatcher.add_handler(COVID_HANDLER)
