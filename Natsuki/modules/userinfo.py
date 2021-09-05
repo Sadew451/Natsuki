dab39cd983d41bcfa7d8846729ab0d45a06c8d81
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 00:37:56 -0700

    Updated

--- Natsuki/modules/userinfo.py
+++ Natsuki/modules/userinfo.py
@@ -13,8 +13,8 @@ from telethon import events
 from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.types import ChannelParticipantsAdmins
 
-import EzilaXBotV.modules.sql.userinfo_sql as sql
-from EzilaXBotV import (
+import Natsuki.modules.sql.userinfo_sql as sql
+from Natsuki import (
     DEMONS,
     DEV_USERS,
     DRAGONS,
@@ -25,14 +25,14 @@ from EzilaXBotV import (
     dispatcher,
     sw,
 )
-from EzilaXBotV import telethn as SaitamaTelethonClient
-from EzilaXBotV.__main__ import STATS, TOKEN, USER_INFO
-from EzilaXBotV.modules.disable import DisableAbleCommandHandler
-from EzilaXBotV.modules.helper_funcs.chat_status import sudo_plus
-from EzilaXBotV.modules.helper_funcs.extraction import extract_user
-from EzilaXBotV.modules.sql.afk_sql import check_afk_status, is_afk
-from EzilaXBotV.modules.sql.global_bans_sql import is_user_gbanned
-from EzilaXBotV.modules.sql.users_sql import get_user_num_chats
+from Natsuki import telethn as SaitamaTelethonClient
+from Natsuki.__main__ import STATS, TOKEN, USER_INFO
+from Natsuki.modules.disable import DisableAbleCommandHandler
+from Natsuki.modules.helper_funcs.chat_status import sudo_plus
+from Natsuki.modules.helper_funcs.extraction import extract_user
+from Natsuki.modules.sql.afk_sql import check_afk_status, is_afk
+from Natsuki.modules.sql.global_bans_sql import is_user_gbanned
+from Natsuki.modules.sql.users_sql import get_user_num_chats
 
 
 def no_by_per(totalhp, percentage):
