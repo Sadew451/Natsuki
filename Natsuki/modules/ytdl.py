c098cb8e6c5b4a1a3d2a9dd41696975933edd8f0
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 00:44:43 -0700

    Updated

--- Natsuki/modules/ytdl.py
+++ Natsuki/modules/ytdl.py
@@ -14,7 +14,7 @@ from youtube_dl.utils import (
     XAttrMetadataError,
 )
 
-from EzilaXBotV.events import register as saitama
+from Natsuki.events import register as saitama
 
 
 @saitama(pattern="^/yt(audio|video) (.*)")
