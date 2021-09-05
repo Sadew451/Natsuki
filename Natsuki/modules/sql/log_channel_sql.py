90913a29a089ed078c9fdaacbab84e09abf6c6e6
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 19:49:59 -0700

    Updated

--- Natsuki/modules/sql/log_channel_sql.py
+++ Natsuki/modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, distinct, func
 
-from EzilaXBotV.modules.sql import BASE, SESSION
+from Natsuki.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
