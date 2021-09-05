7ce28e0ede61011f0c6201887b9f31fb0762bb6d
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 00:31:01 -0700

    Updated

--- Natsuki/modules/telethnfun.py
+++ Natsuki/modules/telethnfun.py
@@ -9,8 +9,8 @@ from bing_image_downloader import downloader
 from requests import get, post
 from telethon.tl import functions, types
 
-from EzilaXBotV import telethn as client
-from EzilaXBotV.events import register
+from Natsuki import telethn as client
+from Natsuki.events import register
 
 
 def progress(current, total):
