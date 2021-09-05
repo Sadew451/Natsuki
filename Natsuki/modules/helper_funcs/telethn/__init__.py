49f037f39721d7f52bede3d1b860d3bfb0cb132d
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 19:31:06 -0700

    Create __init__.py

---
+++ Natsuki/modules/helper_funcs/telethn/__init__.py
@@ -0,0 +1,7 @@
+from Natsuki import DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES, telethn
+
+IMMUNE_USERS = DRAGONS + WOLVES + DEMONS + TIGERS + DEV_USERS
+
+IMMUNE_USERS = (
+    list(DRAGONS) + list(WOLVES) + list(DEMONS) + list(TIGERS) + list(DEV_USERS)
+)
