00308060bf68773755953c99c1fc884f5e6b8b2f
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 00:44:09 -0700

    Updated

--- Natsuki/modules/wiki.py
+++ Natsuki/modules/wiki.py
@@ -3,8 +3,8 @@ from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 from wikipedia.exceptions import DisambiguationError, PageError
 
-from EzilaXBotV import dispatcher
-from EzilaXBotV.modules.disable import DisableAbleCommandHandler
+from Natsuki import dispatcher
+from Natsuki.modules.disable import DisableAbleCommandHandler
 
 
 @run_async
