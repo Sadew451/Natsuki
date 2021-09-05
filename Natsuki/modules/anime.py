db5c85bfd051c8227a33487b6322d2245f587fbe
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 22:49:44 -0700

    Updates

--- Natsuki/modules/anime.py
+++ Natsuki/modules/anime.py
@@ -8,8 +8,8 @@ import requests
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
-from EzilaXBotV import DEV_USERS, DRAGONS, OWNER_ID, dispatcher
-from EzilaXBotV.modules.disable import DisableAbleCommandHandler
+from Natsuki import DEV_USERS, DRAGONS, OWNER_ID, dispatcher
+from Natsuki.modules.disable import DisableAbleCommandHandler
 
 info_btn = "More Information"
 kaizoku_btn = "Kaizoku ☠️"
