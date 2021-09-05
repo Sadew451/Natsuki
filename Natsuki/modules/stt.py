e5c824cce4a08305c7a1d0c6cb1ea1fb6894dbb8
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 00:01:40 -0700

    Updated

--- Natsuki/modules/stt.py
+++ Natsuki/modules/stt.py
@@ -6,9 +6,9 @@ from telethon import *
 from telethon.tl import functions, types
 from telethon.tl.types import *
 
-from EzilaXBotV import *
-from EzilaXBotV import telethn
-from EzilaXBotV.events import register
+from Natsuki import *
+from Natsuki import telethn
+from Natsuki.events import register
 
 
 async def is_register_admin(chat, user):
