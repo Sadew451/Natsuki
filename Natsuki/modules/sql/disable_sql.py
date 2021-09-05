c4c1007737108c0eac481e0d5f9584bea7708b11
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 19:47:33 -0700

    Updated

--- Natsuki/modules/sql/disable_sql.py
+++ Natsuki/modules/sql/disable_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
-from EzilaXBotV.modules.sql import BASE, SESSION
+from Natsuki.modules.sql import BASE, SESSION
 
 
 class Disable(BASE):
