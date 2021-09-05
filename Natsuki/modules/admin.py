5206a7cfe4a2ac533283f594aa9eb18316d922cb
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 22:47:58 -0700

    Updated

--- Natsuki/modules/admin.py
+++ Natsuki/modules/admin.py
@@ -5,10 +5,10 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from EzilaXBotV import DRAGONS, dispatcher
-from EzilaXBotV.modules.disable import DisableAbleCommandHandler
-from EzilaXBotV.modules.helper_funcs.alternate import send_message
-from EzilaXBotV.modules.helper_funcs.chat_status import (
+from Natsuki import DRAGONS, dispatcher
+from Natsuki.modules.disable import DisableAbleCommandHandler
+from Natsuki.modules.helper_funcs.alternate import send_message
+from Natsuki.modules.helper_funcs.chat_status import (
     ADMIN_CACHE,
     bot_admin,
     can_pin,
@@ -16,11 +16,11 @@ from EzilaXBotV.modules.helper_funcs.chat_status import (
     connection_status,
     user_admin,
 )
-from EzilaXBotV.modules.helper_funcs.extraction import (
+from Natsuki.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from EzilaXBotV.modules.log_channel import loggable
+from Natsuki.modules.log_channel import loggable
 
 
 @run_async
