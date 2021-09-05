3419a7e3939f48c704543b29d248ccba62ece12c
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 00:00:56 -0700

    Updated

--- Natsuki/modules/stickers.py
+++ Natsuki/modules/stickers.py
@@ -16,8 +16,8 @@ from telegram import (
 from telegram.ext import CallbackContext, run_async
 from telegram.utils.helpers import mention_html
 
-from EzilaXBotV import dispatcher
-from EzilaXBotV.modules.disable import DisableAbleCommandHandler
+from Natsuki import dispatcher
+from Natsuki.modules.disable import DisableAbleCommandHandler
 
 combot_stickers_url = "https://combot.org/telegram/stickers?q="
 
