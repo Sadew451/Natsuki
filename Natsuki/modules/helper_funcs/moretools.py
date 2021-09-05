2dfe36dbd00a5cf33e27b2d2f0aceec25394c294
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 19:38:48 -0700

    Create moretools.py

---
+++ Natsuki/modules/helper_funcs/moretools.py
@@ -0,0 +1,16 @@
+__help__ = """
+*Text To Speech:*
+⦁ `/tts <text>`*:* convert text to speech.
+*Zipper:*
+⦁ `/zip`*:* reply to a telegram file to compress it in .zip format.
+⦁ `/unzip`*:* reply to a telegram file to decompress it from the .zip format.
+*Zombies:*
+⦁ `/zombies`*:* Check if any deleted accounts available in the group member list and give results.
+*Stickers:*
+⦁ `/stickerid`*:* reply to a sticker to me to tell you its file ID.
+⦁ `/getsticker`*:* reply to a sticker to me to upload its raw PNG file.
+⦁ `/kang`*:* reply to a sticker to add it to your pack.
+⦁ `/stickers`*:* Find stickers for given term on combot sticker catalogue.
+"""
+
+__mod_name__ = "Tools"
