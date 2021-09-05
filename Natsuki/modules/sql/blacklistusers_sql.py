e34e03d1bbbbce150bf6d356826ba23dc6420226
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-04 19:45:11 -0700

    Update blacklistusers_sql.py

--- Natsuki/modules/sql/blacklistusers_sql.py
+++ Natsuki/modules/sql/blacklistusers_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText
 
-from EzilaXBotV.modules.sql import BASE, SESSION
+from Natsuki.modules.sql import BASE, SESSION
 
 
 class BlacklistUsers(BASE):
