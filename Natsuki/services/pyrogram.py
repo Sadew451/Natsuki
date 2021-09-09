9dcafc45c2e5dde3116920f654be3feb3209eb7b
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-09 06:10:41 -0700

    Update pyrogram.py

--- Natsuki/services/pyrogram.py
+++ Natsuki/services/pyrogram.py
@@ -9,7 +9,7 @@ TOKEN = get_str_key("TOKEN", required=True)
 API_ID = get_int_key("API_ID", required=True)
 API_HASH = get_str_key("API_HASH", required=True)
 session_name = TOKEN.split(":")[0]
-Hxy = Client(
+pbot = Client(
     session_name,
     api_id=API_ID,
     api_hash=API_HASH,
@@ -19,4 +19,4 @@ Hxy = Client(
 # disable logging for pyrogram [not for ERROR logging]
 logging.getLogger("pyrogram").setLevel(level=logging.ERROR)
 
-Hxy.start()
+pbot.start()
