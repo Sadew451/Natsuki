ccc749870bce2a583069de2cc45fd5bd88678b61
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-09 06:27:25 -0700

    Update logomaker.py

--- Natsuki/modules/logomaker.py
+++ Natsuki/modules/logomaker.py
@@ -81,7 +81,7 @@ async def lego(event):
        return
     else:
        pass
- await event.reply("Creating your logo...wait...✨")
+ await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
     img = Image.open('./Natsuki/resources/blackbg.jpg')
@@ -100,7 +100,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "NatsukiLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot ❤️")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot ⚡️")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -118,7 +118,7 @@ async def lego(event):
        return
     else:
        pass
- await event.reply("Creating your logo...wait...✨")
+ await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
     img = Image.open('./Natsuki/resources/blackbg.jpg')
@@ -137,14 +137,14 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "NatsukiLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot ❤️")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot ⚡️")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
    await event.reply(f'Error Go to Help From  @NatsukiSupport_Official, {e}')
 
 
-@register(pattern="^/rlogo ?(.*)")
+@register(pattern="^/wlogo ?(.*)")
 async def lego(event):
  quew = event.pattern_match.group(1)
  if event.sender_id == OWNER_ID:
@@ -156,7 +156,7 @@ async def lego(event):
        return
     else:
        pass
- await event.reply("Creating your logo...wait...✨")
+ await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
     img = Image.open('./Natsuki/resources/blackbg.jpg')
@@ -172,15 +172,135 @@ async def lego(event):
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="red", stroke_width=0, stroke_fill="red")
+    draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
     fname2 = "NatsukiLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot ❤️")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
    await event.reply(f'Error Go to Help From  @NatsukiSupport_Official, {e}')
 
+@register(pattern="^/daalogo ?(.*)")
+async def logo_gen(event):
+    xx = await eor(event, get_string("com_1"))
+    name = event.pattern_match.group(1)
+    if not name:
+        await eod(xx, "`Give a name too!`")
+    bg_, font_ = "", ""
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            if hasattr(temp.media, "document"):
+                if "font" in temp.file.mime_type:
+                    font_ = await temp.download_media()
+                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
+                    font_ = await temp.download_media()
+            elif "pic" in mediainfo(temp.media):
+                bg_ = await temp.download_media()
+    else:
+        pics = []
+        async for i in event.client.iter_messages(
+            "@UltroidLogos", filter=InputMessagesFilterPhotos
+        ):
+            pics.append(i)
+        id_ = random.choice(pics)
+        bg_ = await id_.download_media()
+        fpath_ = glob.glob("Natsuki/resources/*")
+        font_ = random.choice(fpath_)
+    if not bg_:
+        pics = []
+        async for i in event.client.iter_messages(
+            "@UltroidLogos", filter=InputMessagesFilterPhotos
+        ):
+            pics.append(i)
+        id_ = random.choice(pics)
+        bg_ = await id_.download_media()
+    if not font_:
+        fpath_ = glob.glob("Natsuki/resources/*")
+        font_ = random.choice(fpath_)
+    if len(name) <= 8:
+        fnt_size = 150
+        strke = 10
+    elif len(name) >= 9:
+        fnt_size = 50
+        strke = 5
+    else:
+        fnt_size = 130
+        strke = 20
+    img = Image.open(bg_)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_, fnt_size)
+    w, h = draw.textsize(name, font=font)
+    h += int(h * 0.21)
+    image_width, image_height = img.size
+    draw.text(
+        ((image_width - w) / 2, (image_height - h) / 2),
+        name,
+        font=font,
+        fill=(255, 255, 255),
+    )
+    x = (image_width - w) / 2
+    y = (image_height - h) / 2
+    draw.text(
+        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
+    )
+    flnme = f"ultd.png"
+    img.save(flnme, "png")
+    await xx.edit("`Done!`")
+    if os.path.exists(flnme):
+        await event.client.send_file(
+            event.chat_id,
+            file=flnme,
+            caption=f"Logo by Natsuki",
+            force_document=True,
+        )
+        os.remove(flnme)
+        await xx.delete()
+    if os.path.exists(bg_):
+        os.remove(bg_)
+    if os.path.exists(font_):
+        if not font_.startswith("Natsuki/resources/"):
+            os.remove(font_)    
+ 
+
+@register(pattern="^/redlogo ?(.*)")
+async def lego(event):
+ quew = event.pattern_match.group(1)
+ if event.sender_id == OWNER_ID:
+     pass
+ else:
+
+    if not quew:
+       await event.reply('Provide Some Text To Draw!')
+       return
+    else:
+       pass
+ await event.reply('Creating your logo...wait!')
+ try:
+    text = event.pattern_match.group(1)
+    img = Image.open('./Natsuki/resources/blackbg.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "gold"
+    shadowcolor = "blue"
+    font = ImageFont.truetype("./Natsuki/resources/Chopsic.otf", 330)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="red")
+    fname2 = "NatsukiLogo.png"
+    img.save(fname2, "png")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot ⚡️")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+ except Exception as e:
+   await event.reply(f'Error Go to Help From  @NatsukiSupport_Official, {e}')
+  
 
 @register(pattern="^/pandalogo ?(.*)")
 async def lego(event):
@@ -194,42 +314,120 @@ async def lego(event):
        return
     else:
        pass
- await event.reply("Creating your logo...wait...✨")
+ await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./Natsuki/resources/pandalogo.jpg')
+    img = Image.open('./Natsuki/resources/pandabg.png')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
+    pointsize = 25
+    fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./Natsuki/resources/Chopsic.otf", 1000)
+    font = ImageFont.truetype("./Natsuki/resources/font.otf", 170)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="yellow", stroke_width=0, stroke_fill="white")
-    fname2 = "NatsukiLogo.png"
+    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
+    fname2 = "NatsukirLogo.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot ❤️")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot ⚡️")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
    await event.reply(f'Error Go to Help From  @NatsukiSupport_Official, {e}')
+  
 
+@register(pattern="^/spiderlogo ?(.*)")
+async def lego(event):
+ quew = event.pattern_match.group(1)
+ if event.sender_id == OWNER_ID:
+     pass
+ else:
 
+    if not quew:
+       await event.reply('Provide Some Text To Draw!')
+       return
+    else:
+       pass
+ await event.reply('Creating your logo...wait!')
+ try:
+    text = event.pattern_match.group(1)
+    img = Image.open('./Natsuki/resources/spiderbg.png')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 25
+    fillcolor = "gold"
+    shadowcolor = "blue"
+    font = ImageFont.truetype("./Natsuki/resources/font.otf", 170)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
+    fname2 = "NatsukiLogo.png"
+    img.save(fname2, "png")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot ⚡️")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+ except Exception as e:
+   await event.reply(f'Error Go to Help From  @NatsukiSupport_Official, {e}')  
+  
+  
+  
+@register(pattern="^/spidermanlogo ?(.*)")
+async def lego(event):
+ quew = event.pattern_match.group(1)
+ if event.sender_id == OWNER_ID:
+     pass
+ else:
+
+    if not quew:
+       await event.reply('Provide Some Text To Draw!')
+       return
+    else:
+       pass
+ await event.reply('Creating your logo...wait!')
+ try:
+    text = event.pattern_match.group(1)
+    img = Image.open('./Natsuki/resources/spiderbg (2).png')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 25
+    fillcolor = "gold"
+    shadowcolor = "blue"
+    font = ImageFont.truetype("./Natsuki/resources/font.otf", 170)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
+    fname2 = "NatsukiLogo.png"
+    img.save(fname2, "png")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @TheNatsukiBot ⚡️")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+ except Exception as e:
+   await event.reply(f'Error Go to Help From  @NatsukiSupport_Official, {e}')  
+  
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
 
 
 __help__ = """
-@TheNatsukiBot
  ❍ /logo text :  Create your logo with your name
  ❍ /biglogo text :  Create your logo Bigger Than `logo`
- ❍ /rlogo text :  Create your logo with your name
- ❍ /pandalogologo text :  Create your logo with your name
+ ❍ /wlogo text : Create your logo with your name
+ ❍ /redlogo text : Create your logo with your name
+ ❍ /pandalogo text : Create your logo with your name
+ ❍ /spiderlogo text : Create your logo with your name
+ ❍ /spidermanlogo text : Create your logo with your name
  """
 __mod_name__ = "Logo Maker"
