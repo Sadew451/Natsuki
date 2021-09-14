9eef389017e22c75de45562eb4ade12b87260345
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-14 18:52:43 +0530

    Update MusicPlay.py

--- Natsuki/modules/MusicPlay.py
+++ Natsuki/modules/MusicPlay.py
@@ -1,27 +1,21 @@
 __help__ = """
 *  • Groups and channels Voice Chat Music Play 🎧  *
-
 1) Make bot admin (Group and in channel if use cplay)
 2) Start a voice chat
 3) Try /play [song name] for the first time by an admin
-4) If userbot joined enjoy music, If not add @EzilaXHelper to your group and retry
-
-* 🟡For Channel Music Play🟡 *
-
-1) Make @EzilaXMBot admin of your channel 
+4) If userbot joined enjoy music, If not add @NatsukiHelper to your group and retry
+* ✨For Channel Music Play✨ *
+1) Make @TheNatsukiBot admin of your channel 
 2) Send /userbotjoinchannel in linked group
 3) Now send commands in linked group 
-
-* ⚪️Groups Music Play⚪️ *
-
+* ✨Groups Music Play✨ *
 • /play: Play song using youtube music
 • /play [yt url] : Play the given yt url
 • /play [reply yo audio]: Play replied audio
 • /dplay: Play song via deezer
 • /splay: Play song via jio saavn
  
- * 🟣Playback🟣 *
-
+ * ✨Playback✨ *
 • /player: Open Settings menu of player
 • /skip: Skips the current track
 • /pause: Pause track
@@ -30,9 +24,7 @@ __help__ = """
 • /current: Shows the current Playing track
 • /playlist: Shows playlist
 Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group. 
-
-* 🔴Channel Music Play🔴 *
-
+* ✨Channel Music Play✨ *
 • /cplay [song name] - play song you requested
 • /cdplay [song name] - play song you requested via deezer
 • /csplay [song name] - play song you requested via jio saavn
@@ -47,24 +39,19 @@ Player cmd and all other cmds except /play, /current  and /playlist  are only fo
 channel is also can be used instead of c ( /cplay = /channelplay )
   
 *  If you donlt like to play in linked group  *
-
 1) Get your channel ID.
 2) Create a group with tittle: Channel Music: your_channel_id
 3) Add bot as Channel admin with full perms
-4) Add @EzilaXMBot to the channel as an admin.
+4) Add @TheNatsukiBot to the channel as an admin.
 5) Simply send commands in your group.
  
-* ⚫️More tools⚫️ *
-
+* ✨More tools✨ *
 • /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-• /userbotjoin: Invite @EzilaXHelper Userbot to your chat 
+• /userbotjoin: Invite @NatsukiHelper Userbot to your chat 
  
-* 🔷Assisten🔷 *
-
-@EzilaXHelper
-
-* 🔸Bot🔸 *
-
-@EzilaXMBot
+* 🙋‍♀️Assisten🙋‍♀️ *
+@NatsukiHelper
+* 🙋‍ Bot 🙋‍♀️ *
+@TheNatsukiBot
 """
 __mod_name__ = "Music Play"
