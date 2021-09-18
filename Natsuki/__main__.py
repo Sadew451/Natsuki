27d76b36d69417ebf7a6c0a4bc8495dbefd99aae
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-18 11:52:13 +0530

    Update __main__.py

--- Natsuki/__main__.py
+++ Natsuki/__main__.py
@@ -95,7 +95,8 @@ buttons = [
 NATSUKI_IMG = "https://telegra.ph/file/a6281ecaae26667a13716.png"
 
 HELP_STRINGS = f"""
-*All Commands :* []()
+*All Commands
+
 🙋‍♀️ Every possibility of Natsuki is documentated here.
 🙋‍♀️ Click buttons to get Help.
 """.format(
