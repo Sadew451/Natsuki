8a13265a2c4f608f03a281e48fc3bf098337095d
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-20 20:26:38 +0530

    Updated

--- Natsuki/modules/krma.py
+++ Natsuki/modules/krma.py
@@ -138,7 +138,7 @@ __help__ = """
 [UPVOTE] - Use upvote keywords like "+", "+1", "thanks" etc to upvote a cb.message.
 [DOWNVOTE] - Use downvote keywords like "-", "-1", etc to downvote a cb.message.
 
-❍ /karma[ON/OFF]: Enable/Disable karma in group. (don't give space)
+❍ /karma [ON/OFF]: Enable/Disable karma in group. (don't give space)
 ❍ /karma [Reply to a message]: Check user's karma
 ❍ /karma: Chek karma list of top 10 users
 """
