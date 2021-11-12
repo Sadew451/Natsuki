f13dc0f2c8d0da05b2b97f47119820e0a13fe702
Sᴀᴅᴇᴡ Jᴀʏᴀsᴇᴋᴀʀᴀ <84616471+Sadew451@users.noreply.github.com>
2021-11-12 17:11:33 +0530

    Update __init__.py

--- Natsuki/modules/sql/__init__.py
+++ Natsuki/modules/sql/__init__.py
@@ -1,16 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-
-from Natsuki import DB_URI
-
+from Natsuki.conf import get_int_key, get_str_key
+from Natsuki import LOGGER
+DB_URI = get_str_key("SQLALCHEMY_DATABASE_URI")
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
+    LOGGER.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 BASE = declarative_base()
-SESSION = start()
+try:
+    SESSION = start()
+except Exception as e:
+    LOGGER.exception(f'[PostgreSQL] Failed to connect due to {e}')
+    exit()
+   
+LOGGER.info("[PostgreSQL] Connection successful, session started.")
